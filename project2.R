a14e7c2d7231b0b09c6210fdabdbf360d8f8701e
Barbara Galiza <barnobregali02@gmail.com>
2025-04-29 20:09:41 +0100

    fixed gap statistcs, organized, added TODOs

--- project2.R
+++ project2.R
@@ -1,66 +1,74 @@
 df = read.csv("~/Documents/UP/24 25/2ºsemestre/EAD/projeto2/fertilizer_recommendation_dataset.csv", sep=",")
+df = read.csv("/home/barbara/MDS/EAD/EAD/fertilizer_recommendation_dataset.csv", sep=",")
 summary(df)
-
-### Clustering
-
-df = df[,1:9]
-df
-
-library(fastDummies)
-one_hot_df <- dummy_cols(df, remove_first_dummy = TRUE, remove_selected_columns = TRUE)
-
-one_hot_df
-head(one_hot_df)
-scaled_df <- scale(one_hot_df)
 
 library(factoextra)
 library(cluster)
 library(fpc)
+library(fastDummies)
+library(mclust)
+library(ggplot2)
 
-#### K-Means clustering ####
+set.seed(123)
 
+############################## Preprocessing ################################
+
+df = df[,1:9]
+df
+
+one_hot_df <- dummy_cols(df, remove_first_dummy = TRUE, remove_selected_columns = TRUE)
+
+dim(one_hot_df)
+head(one_hot_df)
+scaled_df <- scale(one_hot_df)
+summary(scaled_df)
+
+###################### K-Means clustering ########################
+
+# Choosing K
+
+# Elbow method
 fviz_nbclust(scaled_df, kmeans, method = "wss") + 
   theme_minimal() + 
   ggtitle("Elbow Method")
+# k = 6
 
 # Silhouette Method
 fviz_nbclust(scaled_df, kmeans , method = "silhouette") + 
   theme_minimal() + 
   ggtitle("Silhouette Method")
+# k = 5
 
-### Calinski-Harabasz Index (CH)
+# Calinski-Harabasz Index (CH)
 ch_scores <- c()
 for (k in 2:10) {
-  set.seed(123)
   km <- kmeans(scaled_df, centers = k, nstart = 25)
-  cl_stats <- cluster.stats(dist(scaled_df), km$cluster)
+  cl_stats <- cluster.stats(dist(scaled_df), km$cluster) # from fpc library
   ch_scores[k] <- cl_stats$ch
 }
+# nstart = 25 means it tries 25 random initial configurations and chooses the best one (lowest within-cluster sum of squares).
 
-# Remove NAs (porque ch_scores[1] = NA)
-#ch_scores <- ch_scores[!is.na(ch_scores)]
+ch_scores = ch_scores[2:10]
 
 plot(2:10, ch_scores, type = "b", pch = 19,
      xlab = "Número de Clusters K", ylab = "Calinski-Harabasz Index",
      main = "Calinski-Harabasz Index vs K")
-#k=5
+# k = 5
 
-### GAP Statistic
-set.seed(123)
-gap_stat <- clusGap(scaled_df,
-                    FUN = function(x, k) kmeans(x, centers = k, nstart = 25, iter.max = 100),
-                    K.max = 10, B = 50)
+# GAP Statistic
+fviz_nbclust(scaled_df, kmeans , method = "gap_stat", 
+             k.max = 10, 
+             iter.max=100 ) + 
+  theme_minimal() + 
+  ggtitle("Gap Method") 
+# uses  the "1 standard error rule" (firstSEmax) to choose the smallest k such that the gap 
+# is within 1 standard error of the maximum observed gap.
 
-fviz_gap_stat(gap_stat) +
-  theme_minimal() +
-  ggtitle("GAP Statistic Method")
+# Warning message: Quick-TRANSfer stage steps exceeded maximum (= 155000) means one of the boostrapping did not
+# coverge, but it is fine if it is only a few times (it was one)
+# k = 8
 
-maxSE(gap_stat$Tab[, "gap"], 
-      gap_stat$Tab[, "SE.sim"], 
-      method = "firstSEmax")
-#k=10
-
-set.seed(123)
+# Applying Kmeans
 
 cl_5 <- kmeans(scaled_df, centers = 5, nstart = 25)
 
@@ -107,7 +115,8 @@ clusters <- cl_5$cluster
 sil_score <- silhouette(clusters, dist(scaled_df))
 mean(sil_score[, 3])
 
-#### Hierarchical clustering ####
+
+############################ Hierarchical clustering #################################
 
 dist_matrix <- dist(scaled_df)
 hc <- hclust(dist_matrix)
@@ -118,9 +127,7 @@ sil_score <- silhouette(clusters, dist(scaled_df))
 mean(sil_score[, 3])
 
 
-### EM algorithm 
-library(mclust)
-library(ggplot2)
+################### Gaussian Mixture Models (EM algorithm) ###############
 
 #5 clusters
 mod <- Mclust(scaled_df, G=5)
@@ -164,6 +171,7 @@ summary(mod2)
 mod2$G
 
 plot(mod2, what='BIC')
+#plot(mod2, what = "uncertainty")
 
 df <- as.data.frame(scaled_df)
 df $em_cluster <- as.factor(mod2$classification)
@@ -178,3 +186,11 @@ ggplot(scores2, aes(x = Comp.1, y = Comp.2, color = em_cluster)) +
   theme_minimal() +
   labs(title = "EM Clustering com Clusters", x = "PC1", y = "PC2", color = "Cluster")
 
+
+# TODO: 
+# fix GMM clustering always choosing highest number of clusters
+# use inertia to choose number of clusters
+# results of kmeans, see if theres more metrics
+# understand whitening
+# hierarchical clustering - add heatmap?, choose the best distance to cut? results and analysis
+# maybe do some analysis before applying models, like outlier handling
